3ca598af3996a4e21c3ea10d0c40a36bb8b579bb
Elian Valdez <elian.valdez09@gmail.com>
2023-11-05 18:25:41 +0000

    jhjhjhjh

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
 
 
 const firebaseConfig = {
@@ -18,9 +19,11 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 const storage = getStorage(app);
+const analytics = getAnalytics(app);
 
 export {
   db,
   auth,
   storage,
+  analytics
 }
