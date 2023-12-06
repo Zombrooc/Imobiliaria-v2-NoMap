61533e60abf5b52e9b80385d9621c3c32b532d7b
Elian Valdez <elian.valdez09@gmail.com>
2023-12-06 14:30:13 -0300

    asdasdasdasd

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -193,7 +193,11 @@ export default function Navbar() {
                                   <path
                                     fillRule="evenodd"
                                     d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
+<<<<<<< HEAD
                                     clipRule="evenodd"
+=======
+                                    ClipRule="evenodd"
+>>>>>>> 215b6aa (asdasdasdasd)
                                   ></path>
                                 </svg>
                                 Ver todas
